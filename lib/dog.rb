a719c9526363c4d061f44eaef1816e63ee329b0a
RobertObi <robobi97@googlemail.com>
2021-02-26 23:30:43 -0600

    commit tree cac680b8aa8ac68c6016ce783705aaeadf1c8883

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+#Open lib/dog.rb and add a class definition for a Dog class.
+
+class Dog
+    def bark 
+        puts "Woof!"
+    end 
+
+    def sit
+        puts "The Dog is sitting"
+    end 
+end
+
+
